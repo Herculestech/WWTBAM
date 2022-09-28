34b9dac26f0ad9e10c49fd8139eddf53964f2adc
Herculestech <ajalaisaacpromise18@gmail.com>
2022-09-28 11:14:56 -0500

    edit some files

--- adminpage.js
+++ adminpage.js
@@ -1,8 +1,18 @@
 let allQuestion = []
 let inputQuestion 
 function submitQuestion() {
-    if (firstoption.value=="" || secondoption.value=="" || thirdoption.value=="" || questioninput.value=="" || answer.value=="" || fourthoption.value=="") {
-        alert ("input a valid question")
+    if (firstoption.value=="") {
+     disp.innerHTML = `<h1>Fill in all field</h1>`
+    }else if (secondoption.value=="" ){
+        disp.innerHTML = `<h1>Fill in all field</h1>`
+    }else if (thirdoption.value=="") {
+        disp.innerHTML = `<h1>Fill in all field</h1>`
+    }else if (questioninput.value==""){
+        disp.innerHTML = `<h1>Fill in all field</h1>`
+    }else if (answer.value=="") {
+        disp.innerHTML = `<h1>Fill in all field</h1>`
+    }else if (fourthoption.value==""){
+        disp.innerHTML = `<h1>Fill in all field</h1>`
     }else {
      inputQuestion = {
          first_Option:firstoption.value,
@@ -10,11 +20,20 @@ function submitQuestion() {
          thirdOption:thirdoption.value,
          fourthOption:fourthoption.value,
          questionInput:questioninput.value,
-         answerInput:answer.value
+         answerInput:answer.value,
+         userScore: 00
     }
     console.log(inputQuestion)
         allQuestion.push(inputQuestion)
         localStorage.setItem("questionInp", JSON.stringify(allQuestion))
+        disp.innerHTML = `<h1>Question successfully added</h1>`
+        // alert("Question successfully added")
+        firstoption.value=""
+        secondoption.value=""
+        thirdoption.value=""
+        fourthoption.value=""
+        questioninput.value=""
+        answer.value=""
     }
 
 }
--- wwtbam.js
+++ wwtbam.js
@@ -1,9 +1,11 @@
 // let allQuestion 
 next.style.display="none"
+disp.style.display="none"
+label.style.display="none"
 load.style.display="block"
      var mySong = new Audio("AUDIO-2022-09-24-09-28-48.mp3")
      var mySong2 = new Audio("AUDIO-2022-09-24-09-29-36.mp3")
-     mySong.play()
+    //  mySong.play()
 if (localStorage.questionInp){
     allQuestion = JSON.parse(localStorage.getItem("questionInp"))
     // console.log(allQuestion)
@@ -15,21 +17,28 @@ function loadQuestion() {
             optC.innerHTML=item.thirdOption
             optD.innerHTML=item.fourthOption
             mainQuestion.innerHTML=item.questionInput
+            disp.innerHTML= `<div class="text-success" style="font-size: 50px;"> ${item.userScore}</div>`
         })
     next.style.display="block"
     load.style.display="none"
-    mySong2.play()
+    disp.style.display="block"
+    label.style.display="block"
+    // mySong2.play()
     mySong.pause()
 }
 function a(){
     allQuestion.map((item) => {
         correctAnswer=item.answerInput
         firstOpt=item.first_Option
-        console.log(correctAnswer);
+        updatedUserScore=item.userScore
+        // console.log(correctAnswer);
     })
 if (firstOpt==correctAnswer){
     optA.style.border="none"
     optA.style.backgroundColor="green"
+    score = updatedUserScore + 100
+    // console.log(score);
+    disp.innerHTML= `<div class="text-success" style="font-size: 50px;"> ${score}</div>`
   // alert("correct answer")
 }else {
   optA.style.border="none"
@@ -46,11 +55,14 @@ function b(){
     allQuestion.map((item) => {
         correctAnswer=item.answerInput
         secondOpt=item.secondOption
+        updatedUserScore=item.userScore
         // console.log(correctAnswer,secondOpt);
     })
     if (secondOpt==correctAnswer){
+      score= updatedUserScore + 100
       optB.style.border="none"
       optB.style.backgroundColor="green"
+      disp.innerHTML= `<div class="text-success" style="font-size: 50px;"> ${score}</div>`
         // alert("correct answer")
       }else {
         optB.style.border="none"
@@ -66,11 +78,14 @@ function c(){
     allQuestion.map((item) => {
         correctAnswer=item.answerInput
         thirdOpt=item.thirdOption
+        updatedUserScore=item.userScore
         // console.log(correctAnswer);
     })
     if (thirdOpt==correctAnswer){
+      score = updatedUserScore + 100
       optC.style.border="none"
       optC.style.backgroundColor="green"
+      disp.innerHTML= `<div class="text-success" style="font-size: 50px;"> ${score}</div>`
         // console.log(thirdOpt);
         // alert("correct answer")
       }else {
@@ -86,11 +101,15 @@ function d(){
     allQuestion.map((item) => {
         correctAnswer=item.answerInput
         fourthOpt=item.fourthOption
+        updatedUserScore=item.userScore
+        // console.log(Number(item.userScore) + 100);
         // console.log(correctAnswer);
     })
     if (fourthOpt==correctAnswer){
+      score= updatedUserScore + 100
       optD.style.border="none"
       optD.style.backgroundColor="green"
+      disp.innerHTML=`<div class="text-success" style="font-size: 50px;"> ${score}</div>`
         // alert("correct answer")
       }else {
         optD.style.border="none"
@@ -102,5 +121,26 @@ function d(){
     // optA.style.backgroundColor="green"
     // alert("i dey work")
 }
-// let random_question = array[Math.floor(Math.random()* array.length)]
-// if (random_question == item) {
+//  function nextQuest () {
+//     // var allQuestion = [];
+    
+//     // if (allQuestion.length === 0) {
+//     //   for (var i = 0; i < inputQuestion.length; i++) {
+//     //   allQuestion.push(inputQuestion[i]);
+    
+//     //   }
+//     // }
+    
+//     // let item = Math.floor(Math.random() * allQuestion.length);
+// allQuestion.map((item) => {
+//      mainQuestion.innerHTML=item.questionInput
+//      optA.innerHTML=item.first_Option	
+//      optB.innerHTML=item.secondOption
+//      optC.innerHTML=item.fourthOption
+//      optD.innerHTML=item.fourthOption
+// })
+//     //  var i = 0;
+//     //  var score = 0;
+   
+  
+// }
